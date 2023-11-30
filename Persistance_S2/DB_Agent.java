059f48d4488bb071573a79b6418115a17f9d502b
FelipeAlcazar <100356318+FelipeAlcazar@users.noreply.github.com>
2023-11-30 21:39:14 +0100

    Coding It4

---
+++ Persistance_S2/DB_Agent.java
@@ -0,0 +1,15 @@
+package Persistance_S2;
+
+import Persistansce S2.DBAgent;
+
+public class DB_Agent {
+	private DBAgent _myInstance;
+
+	public void change() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void read() {
+		throw new UnsupportedOperationException();
+	}
+}
