28a64832208ea84f2de7c34d02d5cc2d79c1e63f
FelipeAlcazar <100356318+FelipeAlcazar@users.noreply.github.com>
2023-12-05 21:18:06 +0100

    Delete Client_Domain_S2 directory

--- Client_Domain_S2/Event.java
+++
@@ -1,6 +0,0 @@
-package Client_Domain_S2;
-
-public class Event {
-	private String _id;
-	private EventType _type;
-}
--- Client_Domain_S2/Register_Controller.java
+++
@@ -1,8 +0,0 @@
-package Client_Domain_S2;
-
-public class Register_Controller {
-
-	public boolean registerEvent(event aEvent, user aUser) {
-		throw new UnsupportedOperationException();
-	}
-}
