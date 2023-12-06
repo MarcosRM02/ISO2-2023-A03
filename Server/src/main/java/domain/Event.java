60cb5a71803a44a73c8be3dcfeac4d3ecb999c62
Manueehh <72602215+Manueehh@users.noreply.github.com>
2023-12-06 19:45:38 +0100

    Coding it8

---
+++ Server/src/main/java/domain/Event.java
@@ -0,0 +1,33 @@
+package domain;
+
+public class Event {
+	private String _id;
+	private EventType _type;
+	private int _attendance;
+	private int _evaluation;
+	public Calendar _unnamed_Calendar_;
+
+	public void addEvent() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void consultEvent() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void evaluateEvent() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void generatePin() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void generateQR() {
+		throw new UnsupportedOperationException();
+	}
+	
+	public void registerAttendance() {
+		throw new UnsupportedOperationException();
+	}
+}
